7922c82c3a6f2a9e1f0c0ff1a2a5eda4bee30e89
Thando <www.thandwem045@gmail.com>
2024-09-29 13:19:55 +0200

    Updated layout of page

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Fetches background image from  Api
 
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=galaxy')
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=universe')
     .then(res => res.json())
     .then(data => {
         document.getElementById('body').style.backgroundImage = `url(${data.urls.full})`
+        // Gets the author's name.
         document.getElementById('author').textContent = `By: ${data.user.name}`
     })
